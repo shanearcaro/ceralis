10eabd61e4765ffe0fa8fe13dd29a75fb7dd95ab
Shane Arcaro <smarcaro@gmail.com>
2022-11-07 00:20:49 -0500

    Queries now fetch all rows instead of a single row. Result is false on 0 rows returned

--- model/query-database.php
+++ model/query-database.php
@@ -43,15 +43,15 @@ $request_code = $data->{'request'};
 
 switch($request_code) {
     case 0:
-        $query = $pdo->prepare("SELECT user_id, position FROM Users WHERE name = ? AND password= ?");
+        $query = $pdo->prepare("SELECT user_id, position FROM Users WHERE username = ? AND password= ?");
         $query->execute([$data->{'username'}, $data->{'password'}]);
         break;
     case 1:
-        $query = $pdo->prepare("SELECT e.exam_id, e.user_id, e.title, e.points, e.date,
-         se.score, se.date FROM Exams AS e INNER JOIN StudentExams AS se ON e.exam_id=se.exam_id");
+        $query = $pdo->prepare("SELECT e.exam_id, e.user_id, e.title, e.points, e.date, u.name,
+         se.score, se.date FROM Exams AS e INNER JOIN StudentExams AS se ON e.exam_id=se.exam_id INNER JOIN Users AS u ON e.user_id=u.user_id");
         $query->execute();
         break;
 }
 
 $response = $query->fetchAll();
-echo json_encode($response);
+echo json_encode($query->rowCount() == 0 ? false : $response);
