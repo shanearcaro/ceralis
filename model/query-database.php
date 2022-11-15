82e5baaf1395095b34f1547091c76845a0ea1382
egeatay <ege@atay.us>
2022-11-15 02:09:24 -0500

    aesthetic fix

--- model/query-database.php
+++ model/query-database.php
@@ -103,133 +103,151 @@ $request_code = $data->{'request'};
  */
 
 switch($request_code) {
+    
     case 0:
         $query = $pdo->prepare("SELECT user_id, position FROM Users WHERE name = ? AND password= ?");
         $query->execute([$data->{'username'}, $data->{'password'}]);
         break;
 
+
     case 1:
         $query = $pdo->prepare("INSERT INTO Questions (creator_id, question_type, difficulty, constraint, question_text, 
-        tc1, an1, tc2, an2, tc3, an3, tc4, an4, tc5, an5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?. ?)");
+            tc1, an1, tc2, an2, tc3, an3, tc4, an4, tc5, an5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?. ?)");
 
         $query->execute([$data->{'user_id'}, $data->{'questiontype'}, $data->{'difficulty'}, $data->{'constraint'}, 
-        $data->{'question_text'}, $data->{'tc1'}, $data->{'an1'}, $data->{'tc2'}, $data->{'an2'}, $data->{'tc3'}, 
-        $data->{'an3'}, $data->{'tc4'}, $data->{'an4'}, $data->{'tc5'}, $data->{'an5'}]);
+            $data->{'question_text'}, $data->{'tc1'}, $data->{'an1'}, $data->{'tc2'}, $data->{'an2'}, $data->{'tc3'}, 
+            $data->{'an3'}, $data->{'tc4'}, $data->{'an4'}, $data->{'tc5'}, $data->{'an5'}]);
 
         break;
 
+        
     case 2:
         $query = $pdo->prepare("SELECT question_id, created, question_type, difficulty, question_text, tc1, an1, 
-        tc2, an2, tc3, an3, tc4, an4, tc5, an5 FROM Questions WHERE creator_id= ? ");
+            tc2, an2, tc3, an3, tc4, an4, tc5, an5 FROM Questions WHERE creator_id= ? ");
 
         $query->execute([$data->{'user_id'}]);
         break;
+
 
     case 3:
         $query = $pdo->prepare("INSERT INTO Exams (creator_id, title, points, question_count) VALUES (?, ?, ?, ?)");
         $query->execute([$data->{'user_id'}, $data->{'title'}, $data->{'points'}, $data->{'question_count'}]);
         break;
 
+
     case 4:
         $query = $pdo->prepare("INSERT INTO StudentExams (user_id, exam_id, score) VALUES (?, ?, -1)");
         $query->execute([$data->{'user_id'}, $data->{'exam_id'}]);
         break;
 
+
     case 5:
         $query = $pdo->prepare("SELECT q.question_id, q.question_text, eq.questionPoints FROM ExamQuestions as eq 
-        INNER JOIN Exams AS e ON eq.exam_id=e.exam_id 
-        INNER JOIN Questions AS q ON eq.question_id=q.question_id 
-        WHERE e.exam_id= ? ");
+            INNER JOIN Exams AS e ON eq.exam_id=e.exam_id 
+            INNER JOIN Questions AS q ON eq.question_id=q.question_id 
+            WHERE e.exam_id= ? ");
 
         $query->execute([$data->{'exam_id'}]);
         break;
+
 
     case 6:
         $query = $pdo->prepare("UPDATE StudentExams SET score = ? WHERE submission_id= ? ");
         $query->execute([$data->{'score'}, $data->{'submission_id'}]);
         break;
 
+
     case 7:
         $query = $pdo->prepare("UPDATE StudentExams SET score = ? WHERE user_id = ? and exam_id = ? ");
         $query->execute([$data->{'score'}, $data->{'user_id'}, $data->{'exam_id'}]);
         break;
 
+
     case 8:
         $query = $pdo->prepare("SELECT ce.submission_id, ce.question_id, ce.answer, eq.questionPoints, q.question_text, 
-        q.tc1, q.an1, q.tc2, q.an2, q.tc3, q.an3, q.tc4, q.an4, q.tc5, q.an5 FROM CompletedExam AS ce 
-        INNER JOIN StudentExams AS se ON ce.submission_id=se.submission_id 
-        INNER JOIN ExamQuestions AS eq ON se.exam_id=eq.exam_id AND ce.question_id=eq.question_id 
-        INNER JOIN Questions AS q ON ce.question_id=q.question_id 
-        WHERE ce.submission_id= ? ");
+            q.tc1, q.an1, q.tc2, q.an2, q.tc3, q.an3, q.tc4, q.an4, q.tc5, q.an5 FROM CompletedExam AS ce 
+            INNER JOIN StudentExams AS se ON ce.submission_id=se.submission_id 
+            INNER JOIN ExamQuestions AS eq ON se.exam_id=eq.exam_id AND ce.question_id=eq.question_id 
+            INNER JOIN Questions AS q ON ce.question_id=q.question_id 
+            WHERE ce.submission_id= ? ");
 
         $query->execute([$data->{'submission_id'}]);
         break;
+
 
     case 9:
         $query = $pdo->prepare("SELECT name FROM Users WHERE user_id = ? ");
         $query->execute([$data->{'submission_id'}]);
         break;
 
+
     case 10:
         $query = $pdo->prepare("SELECT se.submission_id, se.user_id, se.exam_id, e.score, u.name FROM StudentExams AS se 
-        INNER JOIN Exams AS e on se.exam_id=e.exam_id 
-        INNER JOIN Users AS u on se.user_id=u.user_id 
-        WHERE se.score=-1 AND e.creator_id = ? 
-        ORDER BY se.examID ASC");
+            INNER JOIN Exams AS e on se.exam_id=e.exam_id 
+            INNER JOIN Users AS u on se.user_id=u.user_id 
+            WHERE se.score=-1 AND e.creator_id = ? 
+            ORDER BY se.examID ASC");
 
         $query->execute([$data->{'user_id'}]);
         break;
+
 
     case 11:
         $query = $pdo->prepare("SELECT e.exam_id, e.points, e.question_count, e.creator_id, u.name FROM Exams as e 
-        INNER JOIN Users as u ON u.user_id=e.creator_id 
-        WHERE exam_id NOT IN (
-            SELECT sei.exam_id FROM Exams AS ei JOIN StudentExams AS sei ON ei.exam_id=sei.exam_id 
-            WHERE sei.user_id = ? 
-        )
-        ORDER BY e.exam_id ASC;");
+            INNER JOIN Users as u ON u.user_id=e.creator_id 
+            WHERE exam_id NOT IN (
+                SELECT sei.exam_id FROM Exams AS ei JOIN StudentExams AS sei ON ei.exam_id=sei.exam_id 
+                WHERE sei.user_id = ? 
+            )
+            ORDER BY e.exam_id ASC;");
 
         $query->execute([$data->{'user_id'}]);
         break;
     
+
     case 12:
         $query = $pdo->prepare("SELECT se.submission_id, u.name, se.user_id, se.exam_id, se.score, e.points FROM StudentExams AS se
-        INNER JOIN Exams AS e on se.exam_id=e.exam_id
-        INNER JOIN Users AS u ON e.creator_id=u.user_id
-        WHERE se.score != -1 AND se.user_id = ?
-        ORDER BY se.exam_id ASC");
+            INNER JOIN Exams AS e on se.exam_id=e.exam_id
+            INNER JOIN Users AS u ON e.creator_id=u.user_id
+            WHERE se.score != -1 AND se.user_id = ?
+            ORDER BY se.exam_id ASC");
+        
         $query->execute([$data->{'user_id'}]);
         break;
     
+
     case 13:
         $query = $pdo->prepare("SELECT submission_id FROM StudentExams WHERE user_id = ? AND exam_id = ? ");
         $query->execute([$data->{'user_id'}, $data->{'exam_id'}]);
         break;
+
 
     case 14:
         $query = $pdo->prepare("SELECT question_id FROM ExamQuestions WHERE exam_id= ? ");
         $query->execute([$data->{'exam_id'}]);
         break;
 
+
     case 15: 
         $query = $pdo->prepare("INSERT INTO CompletedExam (submission_id, question_id, answer, result1, result2, 
-        result3, result4, result5, score, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?");
+            result3, result4, result5, score, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?");
 
         $query->execute([$data->{'submission_id'}, $data->{'question_id'}, $data->{'answer'}, 
-        $data->{'result1'}, $data->{'result2'}, $data->{'result3'}, $data->{'result4'}, 
-        $data->{'result5'}, $data->{'score'}, $data->{'comment'}]);
-        
+            $data->{'result1'}, $data->{'result2'}, $data->{'result3'}, $data->{'result4'}, 
+            $data->{'result5'}, $data->{'score'}, $data->{'comment'}]);
+
         break;
     
+
     case 16:
         $query = $pdo->prepare("SELECT ce.submission_id, ce.question_id, ce.answer, ce.result1, ce.result2, ce.result3,
-        ce.result4, ce.result5, ce.score, ce.comment, se.score AS studentGrade, eq.questionPoints, q.question_text, 
-        q.tc1, q.an1, q.tc2, q.an2, q.tc3, q.an3, q.tc4, q.an4, q.tc5, q.an5, e.points FROM CompletedExam AS ce
-        INNER JOIN StudentExams AS se ON ce.submission_id=se.submission_id
-        INNER JOIN ExamQuestions AS eq ON se.exam_id=eq.exam_id AND ce.question_id=eq.question_id
-        INNER JOIN Questions AS q ON ce.question_id=q.question_id
-        INNER JOIN Exams AS e ON e.exam_id=se.exam_id
-        WHERE ce.submission_id = ? ");
+            ce.result4, ce.result5, ce.score, ce.comment, se.score AS studentGrade, eq.questionPoints, q.question_text, 
+            q.tc1, q.an1, q.tc2, q.an2, q.tc3, q.an3, q.tc4, q.an4, q.tc5, q.an5, e.points FROM CompletedExam AS ce
+            INNER JOIN StudentExams AS se ON ce.submission_id=se.submission_id
+            INNER JOIN ExamQuestions AS eq ON se.exam_id=eq.exam_id AND ce.question_id=eq.question_id
+            INNER JOIN Questions AS q ON ce.question_id=q.question_id
+            INNER JOIN Exams AS e ON e.exam_id=se.exam_id
+            WHERE ce.submission_id = ? ");
 
         $query->execute([$data->{'submission_id'}]);
         break;
